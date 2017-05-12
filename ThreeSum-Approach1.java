67d7a634d7ad2a9a259d99783513eec6a3c146a6
Karan Dwivedi <karan1337@live.com>
2017-05-12 00:14:23 -0700

    ThreeSum optimized

--- ThreeSum-Approach1.java
+++ ThreeSum-Approach1.java
@@ -4,43 +4,46 @@ public class ThreeSum{
 	public ArrayList<ArrayList<Integer>> threeSum(ArrayList<Integer> a){
 
 		Collections.sort(a);		
+		System.out.println(a);			
 		
 		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
 		boolean incrementi = false;
 		
 		for(int i=0; i<a.size(); i++){
-			int start=i+1, end=a.size()-1;
-			while(start < end){
-				System.out.println("i="+i+" start="+start+" end="+end);
-				int ni = a.get(i);
-				int nstart = a.get(start);
-				int nend = a.get(end);
-				
-				int sum = ni + nstart + nend;				
-				if(sum == 0){
-					ArrayList<Integer> temp = new ArrayList<Integer>();
-					temp.add(ni);
-					temp.add(nstart);
-					temp.add(nend);
-					if(!res.contains(temp)){
-						res.add(new ArrayList(temp));
+			incrementi = false;			
+			for(int j=a.size()-2; j>i; j--){
+		
+				for(int k=a.size()-1; k>j; k--){
+					System.out.println("i="+i+" j="+j+" k="+k);
+					int ni = a.get(i);
+					int nj = a.get(j);
+					int nk = a.get(k);
+
+					int sum = ni + nj + nk;				
+					if(sum == 0){
+						ArrayList<Integer> temp = new ArrayList<Integer>();
+						temp.add(ni);
+						temp.add(nj);
+						temp.add(nk);
+						if(!res.contains(temp)){
+							res.add(new ArrayList(temp));
+						}
 					}
-					start++;
-					end--;
+					else if(sum > 0){
+						continue;
+					}
+
 				}
 
-				else if(sum < 0){	
-					start++;													}
-				else{
-					end--;		
-				}				
-			}
-		}	
+			}	
+
+		}
 
 		return res;
 	}
 
 	public static void main(String args[])throws Exception{
+
 		ThreeSum ts = new ThreeSum();
 		ArrayList<Integer> arr = new ArrayList<Integer>();
 		arr.add(-1);
