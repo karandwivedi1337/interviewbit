24adcb11104842ba890a00247e25080fd2f81510
Karan Dwivedi <karan1337@live.com>
2017-05-29 15:12:43 -0700

    Merge lists added with heap implementation

--- MergeLists.java
+++ MergeLists.java
@@ -2,6 +2,119 @@ import java.util.*;
 import java.io.*;
 public class MergeLists{
 
+	public class Heap{
+
+
+		int arr[];	
+		int capacity=10;	
+		int pos = 1;
+		Heap(){
+			arr = new int[capacity];
+		}
+
+		public void insertHeap(int ele){		
+
+			//TODO: expand heap size if pos == capacity
+
+			if(pos == capacity){
+				int newcapacity = capacity*2;
+				int newarr[] = new int[newcapacity];
+
+				for(int i=1; i<pos; i++){
+					newarr[i] = arr[i];
+				}
+
+				capacity = newcapacity;
+				arr = newarr;
+			}		
+
+			arr[pos] = ele;
+
+			//percolate upwards
+			int temppos = pos;	
+			while((temppos/2 >= 1) && (temppos < capacity)){
+				if(arr[temppos/2] > ele){
+					arr[temppos] = arr[temppos/2];
+					temppos/=2;	
+				}
+				else{
+					break;
+				}
+			}			
+
+			arr[temppos] = ele;
+			pos++;
+		}	
+
+		public void printHeap(){
+			for(int i=1; i<pos; i++){
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+		}
+
+		public int getMin(){
+
+			if(pos == 2){
+				return arr[--pos];
+			}
+
+			int min = arr[1];
+			arr[1] =  arr[pos-1];
+			arr[pos-1] = min;
+
+			--pos; 	
+
+			//percolate down
+			int temppos = 1;
+			while((temppos*2 < pos) && (temppos < capacity)){			
+
+				//has both left and right child
+				if(temppos*2 + 1 < pos){
+					//left child is smaller than right and left child is < parent
+					if((arr[temppos*2] < arr[temppos*2+1]) && (arr[temppos*2] < arr[temppos])){
+						int temp = arr[temppos*2];
+						arr[temppos*2] = arr[temppos];
+						arr[temppos] = temp;
+						temppos*=2;
+
+					}	
+					//right child is smaller than left and right child is < parent
+					else if((arr[temppos*2+1] < arr[temppos*2]) && (arr[temppos*2+1] < arr[temppos])){
+						int temp = arr[temppos*2+1];
+						arr[temppos*2+1] = arr[temppos];
+						arr[temppos] = temp;
+						temppos=temppos*2+1;
+
+					}
+				}
+
+				//has only left child
+				else if(arr[temppos*2] < arr[temppos]){
+					int temp = arr[temppos*2];
+					arr[temppos*2] = arr[temppos];
+					arr[temppos] = temp;
+					temppos*=2;
+				}
+
+				else{
+					break;
+				}
+			}	
+
+			return min; 
+
+		} 
+
+		public boolean isEmpty(){
+			if(pos <= 1){
+				return true;
+			}
+
+			return false;
+		}	
+
+	}
 	public int getLength(ListNode head){
 
 		int count = 0;
@@ -19,29 +132,63 @@ public class MergeLists{
 
 	public ListNode addNode(ListNode head, int ele){
 
-                if(head == null){
-                        ListNode newnode = new ListNode(ele);
-                        head = newnode;
-                        return head;
-                }
+		if(head == null){
+			ListNode newnode = new ListNode(ele);
+			head = newnode;
+			return head;
+		}
 
-                ListNode temp = head;
+		ListNode temp = head;
 
-                while(temp.next != null){
-                        temp = temp.next;
-                }
+		while(temp.next != null){
+			temp = temp.next;
+		}
 
-                ListNode newnode = new ListNode(ele);
-                temp.next = newnode;
+		ListNode newnode = new ListNode(ele);
+		temp.next = newnode;
 
-                return head;
-        }
+		return head;
+	}
 
+	public boolean isAllNulls(ArrayList<ListNode> a){
+		
+		for(int i=0; i<a.size(); i++){
+			if(a.get(i) != null){
+				return false;
+			}
+		}
+		return true;
+	}
 
 	public ListNode mergeKLists(ArrayList<ListNode> a) {
 
-		ListNode res = null;	
+		ListNode res = null;
+		Heap h = new Heap();
+
 		for(int i=0; i<a.size(); i++){
+			if(a.get(i) != null){
+				h.insertHeap(a.get(i).val);
+			}
+		}	
+	
+		while(!isAllNulls(a)){
+					
+			int min = h.getMin();			
+			res = addNode(res, min);			
+			for(int i=0; i<a.size(); i++){
+
+				if(a.get(i) != null){
+					int aval = a.get(i).val;
+					if(aval == min){
+						ListNode nextnode = a.get(i).next;
+						a.set(i, a.get(i).next);
+						if(nextnode != null){
+							h.insertHeap(nextnode.val);
+						}	
+					}
+				}
+			}
+
 		}
 		return res;
 	}	
