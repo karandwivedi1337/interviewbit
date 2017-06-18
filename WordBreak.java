8562388ad990f17d238753c97230d528a6b769fe
Karan Dwivedi <karan1337@live.com>
2017-06-17 23:16:54 -0700

    WordBreak DP added

--- WordBreak.java
+++ WordBreak.java
@@ -5,6 +5,7 @@ public class WordBreak{
 	int state = 0;
 	HashMap<String, String> hmap;
 
+	/*
 	public void recur(String a){
 
 		System.out.println("recur called with a=" + a);
@@ -16,7 +17,9 @@ public class WordBreak{
 
 		if(a.length() > 0){
 			for(int i=0; i<a.length(); i++){
-
+				if(state == 1){
+					break;
+				}
 				String substr = a.substring(0, i+1);
 				System.out.println("substr=" + substr);
 				if(hmap.containsKey(substr)){
@@ -27,6 +30,46 @@ public class WordBreak{
 
 		}
 
+	}
+	*/
+
+	public void printArr(boolean[] dp){
+
+		for(int i=0; i<dp.length; i++){
+			if(dp[i] == true){
+				System.out.print("1 ");
+			}
+
+			else{
+				System.out.print("0 ");
+			}
+		}
+		System.out.println();
+	}
+
+	public void recur(String a){
+
+		boolean dp[] = new boolean[a.length()+1];
+		for(int i=0; i<dp.length; i++){
+			dp[i] = false;
+		}
+		
+		dp[0] = true;	
+
+		for(int i=1; i<=a.length(); i++){
+			for(int j=0; j<i; j++){
+				if(dp[j] && hmap.containsKey(a.substring(j, i))){
+					dp[i] = true;
+					printArr(dp);
+			}
+			}
+		}				
+		
+		if(dp[a.length()] == true){
+			state =1;
+		}
+	
+		
 	}
 
 	public int wordBreak(String a, ArrayList<String> b) {
@@ -42,6 +85,6 @@ public class WordBreak{
 	public static void main(String args[])throws Exception{
 		WordBreak wb = new WordBreak();	
 		String a = "myinterviewtrainer";
-		ArrayList<String> b = new ArrayList<>(Arrays.asList("interview", "my", "trainer"));	
+		ArrayList<String> b = new ArrayList<>(Arrays.asList("interview", "my", "trainer", "m"));	
 		System.out.println(wb.wordBreak(a,b));
 	}}
